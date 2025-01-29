503c4e825824b9644044409ee5c167c4699fb579
BrenoCyrino <breno.cyrino@usp.br>
2025-01-29 20:46:08 -0300

    Add files via upload

---
+++ buttonDownload.js
@@ -0,0 +1,6 @@
+var buttonDownload = document.createElement('button');
+
+buttonDownload.innerHTML = 'TT';
+buttonDownload.setAttribute('type', 'button');
+buttonDownload.setAttribute('style', 'margin: 15px;');
+
---
+++ main.js
@@ -0,0 +1,3 @@
+var menu = document.getElementById('table');
+
+menu.appendChild(buttonDownload);
